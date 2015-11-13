25d24e0275820129fd8de2ba7c1b6c1c1dd61717
Daniel Durazo <mfl.double.d@gmail.com>
2015-11-13 02:52:52 -0700

    Hopefully fixed the indent issues with vim...

--- client.c
+++ client.c
@@ -26,50 +26,50 @@ int main(int argc, char** argv){
 	// Create the socket
 	if ((client_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1){
 		perror("Socket error\n");
-	    exit(1);
-    }
-
-	// Set up the address
-    memset((char *)&server_address, 0, sizeof(server_address));
-    server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(PORT);
-
-	// Internet address manipulation?
-    if (inet_aton(SERVER_ADDR, &server_address.sin_addr) == 0){
-    	perror("inet_aton() failed\n");
-    	exit(1);
-    }
-
-    // Keep allowing user to choose, choice of 4 will exit, nice visual menu for easy understanding
-    while(1){
-        printf("==========Welcome to the Datagram programming assignment!==========\n");
-        printf("=         Enter 1 to use the echo server.                         =\n");
-        printf("=         Enter 2 to use the DNS lookup service.                  =\n");
-        printf("=         Enter 3 to get the server time.                         =\n");
-        printf("=         Enter 4 to exit.                                        =\n");
-        printf("===================================================================\n");
-        printf("Enter your choice: ");
-        scanf("%d", &userChoice);
-        
-	if(userChoice == 1) { 
-		// Let user send a message to the server
-		echoServer(client_socket, server_address, slen); 
+		exit(1);
 	}
 
-        else if(userChoice == 2) {
-		// Let user get the IP for their requested URL
-		dnsLookup(client_socket, server_address, slen);
-        }
-        else if(userChoice == 3) {
-		// Get the server time
-            serverTime(client_socket, server_address, slen);
-        }
-	// Exit the client
-        else if(userChoice == 4) return 0;
+	// Set up the address
+	memset((char *)&server_address, 0, sizeof(server_address));
+	server_address.sin_family = AF_INET;
+	server_address.sin_port = htons(PORT);
 
-	// Invalid input/choice
-        else printf("You must enter either 1, 2, 3, or 4.\n");
-    }
+	// Internet address manipulation?
+	if (inet_aton(SERVER_ADDR, &server_address.sin_addr) == 0){
+		perror("inet_aton() failed\n");
+		exit(1);
+	}
+
+	// Keep allowing user to choose, choice of 4 will exit, nice visual menu for easy understanding
+	while(1){
+		printf("==========Welcome to the Datagram programming assignment!==========\n");
+		printf("=         Enter 1 to use the echo server.                         =\n");
+		printf("=         Enter 2 to use the DNS lookup service.                  =\n");
+		printf("=         Enter 3 to get the server time.                         =\n");
+		printf("=         Enter 4 to exit.                                        =\n");
+		printf("===================================================================\n");
+		printf("Enter your choice: ");
+		scanf("%d", &userChoice);
+
+		if(userChoice == 1) { 
+			// Let user send a message to the server
+			echoServer(client_socket, server_address, slen); 
+		}
+
+		else if(userChoice == 2) {
+			// Let user get the IP for their requested URL
+			dnsLookup(client_socket, server_address, slen);
+		}
+		else if(userChoice == 3) {
+			// Get the server time
+			serverTime(client_socket, server_address, slen);
+		}
+		// Exit the client
+		else if(userChoice == 4) return 0;
+
+		// Invalid input/choice
+		else printf("You must enter either 1, 2, 3, or 4.\n");
+	}
 }
 
 // Pass in the relevant socket/address information to be able to send datagrams
@@ -85,35 +85,35 @@ void echoServer(int client_socket, struct sockaddr_in server_address, socklen_t 
 	buf[0] = '1';
 
 	// Send the message to the server, display error if something went wrong
-    	if (sendto(client_socket, buf, sizeof(buf), 0, (struct sockaddr *)&server_address, slen) == -1){
-    		perror("sendto");
-    		exit(1);
-        }
+	if (sendto(client_socket, buf, sizeof(buf), 0, (struct sockaddr *)&server_address, slen) == -1){
+		perror("sendto");
+		exit(1);
+	}
 
 	// Clear the buffer for safe usage (less risk of garbage data or previous long message)
 	memset(buf, '\0', sizeof(buf));
 
 	// Grab a string from the user and place it after the initial character
-    	printf("Enter message: ");
-    	scanf(" %[^\n]%*c", buf);
+	printf("Enter message: ");
+	scanf(" %[^\n]%*c", buf);
 
-    	// Send the message to the server, display error if something went wrong
-    	if (sendto(client_socket, buf, sizeof(buf), 0, (struct sockaddr *)&server_address, slen) == -1){
-    		perror("sendto");
-    		exit(1);
-        }
+	// Send the message to the server, display error if something went wrong
+	if (sendto(client_socket, buf, sizeof(buf), 0, (struct sockaddr *)&server_address, slen) == -1){
+		perror("sendto");
+		exit(1);
+	}
 
 	// Clear the buffer again for safe usage
-        memset(buf, '\0', sizeof(buf));
+	memset(buf, '\0', sizeof(buf));
 
-        // Receive the echo'd message from the server
-        if((recvfrom(client_socket, buf, sizeof(buf), 0, NULL, NULL)) == -1){
-            perror("Receive error");
-            exit(1);
-        }
+	// Receive the echo'd message from the server
+	if((recvfrom(client_socket, buf, sizeof(buf), 0, NULL, NULL)) == -1){
+		perror("Receive error");
+		exit(1);
+	}
 
 	// Print the received echo message to show it worked!
-        printf("Received from server: %s\n", buf);
+	printf("Received from server: %s\n", buf);
 }
 
 void dnsLookup(int client_socket, struct sockaddr_in server_address, socklen_t slen){
@@ -128,40 +128,40 @@ void dnsLookup(int client_socket, struct sockaddr_in server_address, socklen_t s
 	buf[0] = '2';
 
 	// Send the message to the server, display error if something went wrong
-    	if (sendto(client_socket, buf, sizeof(buf), 0, (struct sockaddr *)&server_address, slen) == -1){
-    		perror("Error sending to server\n");
-    		exit(1);
-        }
+	if (sendto(client_socket, buf, sizeof(buf), 0, (struct sockaddr *)&server_address, slen) == -1){
+		perror("Error sending to server\n");
+		exit(1);
+	}
 
 	// Clear the buffer for safe usage (less risk of garbage data or previous long message)
 	memset(buf, '\0', sizeof(buf));
 
 	// Grab a string from the user and place it after the initial character
-    	printf("Enter URL to IP lookup: ");
-    	scanf(" %[^\n]%*c", buf);
+	printf("Enter URL to IP lookup: ");
+	scanf(" %[^\n]%*c", buf);
 
-    	// Send the message to the server, display error if something went wrong
-    	if (sendto(client_socket, buf, sizeof(buf), 0, (struct sockaddr *)&server_address, slen) == -1){
-    		perror("Error sending to server");
-    		exit(1);
-        }
+	// Send the message to the server, display error if something went wrong
+	if (sendto(client_socket, buf, sizeof(buf), 0, (struct sockaddr *)&server_address, slen) == -1){
+		perror("Error sending to server");
+		exit(1);
+	}
 
 	// Clear the buffer again for safe usage
-        memset(buf, '\0', sizeof(buf));
+	memset(buf, '\0', sizeof(buf));
 
-        // Receive the echo'd message from the server
-        if((recvfrom(client_socket, buf, sizeof(buf), 0, NULL, NULL)) == -1){
-            perror("Receive error");
-            exit(1);
-        }
+	// Receive the echo'd message from the server
+	if((recvfrom(client_socket, buf, sizeof(buf), 0, NULL, NULL)) == -1){
+		perror("Receive error");
+		exit(1);
+	}
 
 	// Print the received echo message to show it worked!
-        printf("The address resolved to: %s\n", buf);
+	printf("The address resolved to: %s\n", buf);
 
 }
 
 void serverTime(int client_socket, struct sockaddr_in server_address, socklen_t slen){
-    
+
 	// Create a character buffer (for a string) for receiving
 	char buf[BUFLEN];
 
@@ -171,23 +171,23 @@ void serverTime(int client_socket, struct sockaddr_in server_address, socklen_t 
 	// Set the first character to let server know to send time back
 	buf[0] = '3';
 
-    	// Send the message to the server, display error if something went wrong
-    	if (sendto(client_socket, buf, sizeof(buf), 0, (struct sockaddr *)&server_address, slen) == -1){
-    		perror("Error sending time request to server");
-    		exit(1);
-        }
+	// Send the message to the server, display error if something went wrong
+	if (sendto(client_socket, buf, sizeof(buf), 0, (struct sockaddr *)&server_address, slen) == -1){
+		perror("Error sending time request to server");
+		exit(1);
+	}
 
 	// Clear the buffer again for safe usage
-        memset(buf, '\0', sizeof(buf));
+	memset(buf, '\0', sizeof(buf));
 
-        // Receive the time message from the server
-        if((recvfrom(client_socket, buf, sizeof(buf), 0, NULL, NULL)) == -1){
-            perror("Receive error");
-            exit(1);
-        }
+	// Receive the time message from the server
+	if((recvfrom(client_socket, buf, sizeof(buf), 0, NULL, NULL)) == -1){
+		perror("Receive error");
+		exit(1);
+	}
 
 	// Print the received echo message to show it worked!
-        printf("Time from server: %s\n", buf);
+	printf("Time from server: %s\n", buf);
 
 }
 
--- server.c
+++ server.c
@@ -14,7 +14,7 @@ void dnsLookup(int, struct sockaddr_in, socklen_t);
 
 
 int main( int argc, char** argv) {
-	
+
 	// Creating required variables for UDP socket
 	struct sockaddr_in server_address, client_address;
 	int server_socket;
@@ -33,91 +33,91 @@ int main( int argc, char** argv) {
 		exit(1);
 	}
 
-     // Zero out the structure
-     memset((char *) &server_address, 0, sizeof(server_address));
+	// Zero out the structure
+	memset((char *) &server_address, 0, sizeof(server_address));
 
 	// Setting address
-     server_address.sin_family = AF_INET;
-     server_address.sin_addr.s_addr = htonl(INADDR_ANY);
-     server_address.sin_port = htons(PORT);
+	server_address.sin_family = AF_INET;
+	server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+	server_address.sin_port = htons(PORT);
 
-     //bind socket to port
-     if(bind(server_socket, (struct sockaddr*)&server_address, sizeof(server_address)) == -1){
-     	 perror("Error binding socket.");
-     	 exit(1);
-     }
-
-     //keep listening for data
-     while(!0){
-
-	// Initially waiting for datagram
-     	 printf("Waiting for data...\n");
-
-	// Clear out the buffer memory for safe usage each time
-         memset(buf, '\0', sizeof(buf));
-
-     	 //try to recieve some data, this is a blocking call, show error if something goes wrong
-     	 if ((recv_len = recvfrom(server_socket, buf, sizeof(buf), 0, (struct sockaddr *)&client_address, &client_address_len)) == -1)
-         {
-         	 perror("Error in recvfrom");
-         	 exit(1);
-         }
-	 
-	// Check for the requested service to send back to client (first char of received message)
-	// 1 = Echo a message
-	// 2 = URL to IP lookup
-	// 3 = Get current time
-	
-	choice = buf[0];
-
-	// Choice 1: Echo
-	if(choice == '1') {
-
-		// For each case, let a function handle what needs to be done
-		echoMessage(server_socket, client_address, client_address_len);
-
-	} 
-	// Choice 2: DNS Lookup 
-	else if(choice == '2') {
-		
-		// Pass in the socket and address to be able to send datagrams back to client
-		dnsLookup(server_socket, client_address, client_address_len);
-
-	} 
-	// Choice 3: Get current time
-	else {
-		
-		getTime(server_socket, client_address, client_address_len);
+	//bind socket to port
+	if(bind(server_socket, (struct sockaddr*)&server_address, sizeof(server_address)) == -1){
+		perror("Error binding socket.");
+		exit(1);
 	}
 
-     }
+	//keep listening for data
+	while(!0){
 
-     close(server_socket);
-     return 0;
+		// Initially waiting for datagram
+		printf("Waiting for data...\n");
+
+		// Clear out the buffer memory for safe usage each time
+		memset(buf, '\0', sizeof(buf));
+
+		//try to recieve some data, this is a blocking call, show error if something goes wrong
+		if ((recv_len = recvfrom(server_socket, buf, sizeof(buf), 0, (struct sockaddr *)&client_address, &client_address_len)) == -1)
+		{
+			perror("Error in recvfrom");
+			exit(1);
+		}
+
+		// Check for the requested service to send back to client (first char of received message)
+		// 1 = Echo a message
+		// 2 = URL to IP lookup
+		// 3 = Get current time
+
+		choice = buf[0];
+
+		// Choice 1: Echo
+		if(choice == '1') {
+
+			// For each case, let a function handle what needs to be done
+			echoMessage(server_socket, client_address, client_address_len);
+
+		} 
+		// Choice 2: DNS Lookup 
+		else if(choice == '2') {
+
+			// Pass in the socket and address to be able to send datagrams back to client
+			dnsLookup(server_socket, client_address, client_address_len);
+
+		} 
+		// Choice 3: Get current time
+		else {
+
+			getTime(server_socket, client_address, client_address_len);
+		}
+
+	}
+
+	close(server_socket);
+	return 0;
 }
 
 void echoMessage(int server_socket, struct sockaddr_in client_address, socklen_t client_address_len) {
-	
+
 	// Buffer for reading message from client
 	char buf[BUFLEN];
 	int recv_len;
 
 	// Try to receive the message from the client, blocking call, show error if something goes wrong
-	 if ((recv_len = recvfrom(server_socket, buf, sizeof(buf), 0, (struct sockaddr *)&client_address, &client_address_len)) == -1)
-         {
-         	 perror("Error in recvfrom");
-         	 exit(1);
-         }
+	if ((recv_len = recvfrom(server_socket, buf, sizeof(buf), 0, (struct sockaddr *)&client_address, &client_address_len)) == -1)
+	{
+		perror("Error in recvfrom");
+		exit(1);
+	}
 
 	// Display server side message for echo request
 	printf("Client requested to echo: %s\n\n", buf);
 
 	// Send the message back to the client, show error if something goes wrong
 	if (sendto(server_socket, buf, recv_len, 0, (struct sockaddr *)&client_address, client_address_len) == -1)
-         {
-         	 perror("Error sending message to client\n");
-         	 exit(1);
-         }
+	{
+		perror("Error sending message to client\n");
+		exit(1);
+	}
 }
 
 
@@ -138,57 +138,57 @@ void getTime(int server_socket, struct sockaddr_in client_address, socklen_t cli
 
 	// Send the time message back to the client, show error if something goes wrong
 	if (sendto(server_socket, timeString, sizeof(timeString), 0, (struct sockaddr *)&client_address, client_address_len) == -1)
-         {
-         	 perror("Error sending message to client\n");
-         	 exit(1);
-         }
+	{
+		perror("Error sending message to client\n");
+		exit(1);
+	}
 }
 
 
 void dnsLookup(int server_socket, struct sockaddr_in client_address, socklen_t client_address_len) {
-    // Struct for our address info
-    struct addrinfo *result, *res;
-    struct sockaddr_in *h;
+	// Struct for our address info
+	struct addrinfo *result, *res;
+	struct sockaddr_in *h;
 
-    // Our buffer and integer for received bytes
+	// Our buffer and integer for received bytes
 	char buf[BUFLEN];
 	int recv_len;
 
-    // Clear the buffer
-    memset(buf, '\0', BUFLEN);
+	// Clear the buffer
+	memset(buf, '\0', BUFLEN);
 
 	// Try to receive the message from the client, blocking call, show error if something goes wrong
-	 if ((recv_len = recvfrom(server_socket, buf, sizeof(buf), 0, (struct sockaddr *)&client_address, &client_address_len)) == -1)
-         {
-         	 perror("Error in recvfrom");
-         	 exit(1);
-         }
+	if ((recv_len = recvfrom(server_socket, buf, sizeof(buf), 0, (struct sockaddr *)&client_address, &client_address_len)) == -1)
+	{
+		perror("Error in recvfrom");
+		exit(1);
+	}
 
 	//this is to hold the addrinfo
-    int status = getaddrinfo(buf, NULL, NULL, &result);
+	int status = getaddrinfo(buf, NULL, NULL, &result);
 
 
-    //This determines what to do based on if there is an error with getaddrinfo
-    if(status == 0){
-        //get the ip addr from the struct
-        h = (struct sockaddr_in*)result->ai_addr;
-        strcpy(buf, inet_ntoa(h->sin_addr));
+	//This determines what to do based on if there is an error with getaddrinfo
+	if(status == 0){
+		//get the ip addr from the struct
+		h = (struct sockaddr_in*)result->ai_addr;
+		strcpy(buf, inet_ntoa(h->sin_addr));
 
-        //we need to free the memory space
-        freeaddrinfo(result);
-    }else{
-        //if this happens then there is an error
-        perror("getaddrinfo error\n");
-        strcpy(buf, "getaddrinfo error\n");
-    }
+		//we need to free the memory space
+		freeaddrinfo(result);
+	}else{
+		//if this happens then there is an error
+		perror("getaddrinfo error\n");
+		strcpy(buf, "getaddrinfo error\n");
+	}
 
-    //Now we are going to send what is in the buffer (either the error message or the IP address)
-    if(sendto(server_socket, buf, sizeof(buf), 0, (struct sockaddr*)&client_address, client_address_len) == -1){
-        perror("sendto");
-        exit(1);
-    }
+	//Now we are going to send what is in the buffer (either the error message or the IP address)
+	if(sendto(server_socket, buf, sizeof(buf), 0, (struct sockaddr*)&client_address, client_address_len) == -1){
+		perror("sendto");
+		exit(1);
+	}
 
-    //Now we want to print out the buffer on the server side
-    printf("Sending IP address %s\n", buf);
+	//Now we want to print out the buffer on the server side
+	printf("Sending IP address %s\n", buf);
 }
 
